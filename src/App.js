33785e6bbeefc63c2233fcd6170f3e1b7c03c7f7
Ranvirdeshmukh <ranvir.r.deshmukh.26@dartmouth.edu>
2025-04-17 23:58:19 -0400

    added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import ResumePage from './pages/ResumePage';
+import { pageView } from './analytics';
 import './App.css';
+
+// Analytics tracker component
+function PageViewTracker() {
+  const location = useLocation();
+  
+  useEffect(() => {
+    pageView(location.pathname);
+  }, [location]);
+  
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <PageViewTracker />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/resume" element={<ResumePage />} />
