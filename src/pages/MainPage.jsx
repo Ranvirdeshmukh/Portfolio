493cacf358a30c5db58fda168bbada4b094fdf29
Ranvirdeshmukh <ranvir.r.deshmukh.26@dartmouth.edu>
2025-12-06 10:57:17 -0500

    added

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -194,7 +194,7 @@ const MinimalPortfolio = () => {
           </p>
 
           <p style={socialLinksStyle}>
-            You can find my experiences on <a href="https://www.linkedin.com/in/ranvir-deshmukh-209706199/" target="_blank" rel="noopener noreferrer" style={linkStyle}>LinkedIn</a>, my witty side on <a href="https://x.com/ranvirdeshmukh_" target="_blank" rel="noopener noreferrer" style={linkStyle}>X</a>, and my code on <a href="https://github.com/Ranvirdeshmukh" target="_blank" rel="noopener noreferrer" style={linkStyle}>GitHub</a>.
+            You can find my experiences on <a href="https://www.linkedin.com/in/ranvir-deshmukh-209706199/" target="_blank" rel="noopener noreferrer" style={linkStyle}>LinkedIn</a>, my thoughts side on <a href="https://x.com/ranvirdeshmukh_" target="_blank" rel="noopener noreferrer" style={linkStyle}>X</a>, and my code on <a href="https://github.com/Ranvirdeshmukh" target="_blank" rel="noopener noreferrer" style={linkStyle}>GitHub</a>.
           </p>
           
           <div style={logoStyle}>
